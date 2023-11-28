8168205697e8aa2ef561c7b41383afc4b0272159
김선욱 <tjsdnr960@naver.com>
2023-11-28 15:56:40 +0900

    React 복습

---
+++ ReactJS/20231128/react-for-beginners/src/App.js
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1)
+  console.log('call an api')
+  useEffect()
+  return (
+    <div>
+      <h1>{ counter }</h1>
+      <button onClick={onClick}>click me!</button>
+    </div>
+  );
+}
+
+export default App;
