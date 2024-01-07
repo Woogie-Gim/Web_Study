4d933d148071c90e8b84cb9611b37dfb6146f3b6
김선욱 <tjsdnr960@naver.com>
2024-01-07 17:24:11 +0900

    React-Redux / Redux-toolkit

---
+++ ReactJS/20240107/react-redux/src/App.js
@@ -0,0 +1,20 @@
+import { React } from 'react';
+import { HashRouter as Router } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
+import Home from './routes/Home';
+import Detail from './routes/Detail';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<Home />}></Route>
+          <Route path="/:id" element={<Detail />}></Route>
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App;
